5faf30eb401094296252551f5a06cdc6cf9e0247
bogdanarizancu <b-t.arizancu@metro-markets.de>
2023-07-05 10:21:37 +0200

    wip

---
+++ plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+  Plugin Name: WP Glossary Synonyms
+  Version: 1.0
+  Text Domain: wp-glossary-synonyms
+  Description: A custom plugin extension for WP Glossary plugin
+  Author: Bogdan Arizancu
+  Author URI: https://github.com/bogdanarizancu
+  License: GPL-2.0+
+  License URI: https://www.gnu.org/licenses/gpl-2.0
+*/
+
+namespace WPGlossarySynonyms;
+
+if (!defined('ABSPATH')) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+  exit;
+}
+
+/**
+ * Loads PSR-4-style plugin classes.
+ */
+function classloader($class) {
+  static $ns_offset;
+  if (strpos($class, __NAMESPACE__ . '\\') === 0) {
+    if ($ns_offset === NULL) {
+      $ns_offset = strlen(__NAMESPACE__) + 1;
+    }
+    include __DIR__ . '/src/' . strtr(substr($class, $ns_offset), '\\', '/') . '.php';
+  }
+}
+spl_autoload_register(__NAMESPACE__ . '\classloader');
+
+register_activation_hook(__FILE__, __NAMESPACE__ . '\Schema::activate');
+register_deactivation_hook(__FILE__, __NAMESPACE__ . '\Schema::deactivate');
+register_uninstall_hook(__FILE__, __NAMESPACE__ . '\Schema::uninstall');
+
+$plugin = new Plugin();
+add_action('plugins_loaded', [$plugin, 'loadTextdomain']);
+add_action('init', [$plugin, 'init'], 20);
---
+++ src/Schema.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains \WPGlossarySynonyms\Schema.
+ */
+
+namespace WPGlossarySynonyms;
+
+/**
+ * Plugin lifetime and maintenance.
+ */
+class Schema
+{
+
+	/**
+	 * register_activation_hook() callback.
+	 */
+	public static function activate()
+	{
+		// Ensure parent plugin exists and is activated.
+		if (!is_plugin_active('wp_glossary/index.php') and current_user_can('activate_plugins')) {
+			// Stop activation redirect and show error
+			wp_die('Sorry, but this plugin requires the WP Glossary Plugin to be installed and active. <br><a href="' . admin_url('plugins.php') . '">&laquo; Return to Plugins</a>');
+		}
+	}
+
+	/**
+	 * register_deactivation_hook() callback.
+	 */
+	public static function deactivate()
+	{
+	}
+
+	/**
+	 * register_uninstall_hook() callback.
+	 */
+	public static function uninstall()
+	{
+	}
+}
